541ad82ce24ac494ba8b2617fe16aed02b733a74
Mr.A.L.M.Aslam <almaslam01@gmail.com>
2023-06-29 21:59:16 +0530

    Studnet service class created

--- src/main/java/com/studentmanagemnt/Service/StudentService.java
+++ src/main/java/com/studentmanagemnt/Service/StudentService.java
@@ -1,5 +1,23 @@
 package com.studentmanagemnt.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studentmanagemnt.Entity.Student;
+import com.studentmanagemnt.Repository.StudentRepo;
+
+@Service
+
 public class StudentService {
+	@Autowired
+	private StudentRepo studentRepo;
+	
+	public Student saveDetails(Student student) {
+		
+		return studentRepo.save(student);
+		
+	}
+	
+	
 
 }
